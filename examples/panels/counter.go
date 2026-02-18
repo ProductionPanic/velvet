98e0808b30a87a093b6f802fb91c9fcec518941d
freekattema <freek@bureauzigzag.nl>
2026-02-18 17:30:11 +0100

    changes

--- examples/panels/counter.go
+++ examples/panels/counter.go
@@ -35,7 +35,6 @@ func (c CounterComponent) View() *velvet.Container {
 	output := velvet.NewContainer().
 		Foreground(color.FromHex("#fff")).
 		Background(color.FromHex("#222")).
-		Padding(1, 2).
 		Border(style.RoundedBorder()).
 		BorderColor(color.FromHex("#555")).
 		Width(c.Width).
@@ -49,10 +48,6 @@ func (c CounterComponent) View() *velvet.Container {
 	var sb strings.Builder
 
 	sb.WriteString(fmt.Sprintf("Counter: %d\n", c.Value))
-	sb.WriteString("Use Up/Down to change the counter.\n")
-	sb.WriteString("Press Space to reset.\n")
-	sb.WriteString("Press Tab to switch panels.\n")
-	sb.WriteString("Press Ctrl+C or Esc to quit.")
 
 	output.Content(sb.String())
 	return output
